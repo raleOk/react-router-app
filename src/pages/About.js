2fc02a293470a916d697e7b8567212aaeec884fa
Rastko Zivkovic <rale.pablito@gmail.com>
2021-12-24 19:45:34 +0100

    added basic styling and material icons

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Typography } from "@mui/material";
 
 const About = () => {
-  return <div>about</div>;
+  return (
+    <Typography>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </Typography>
+  );
 };
 
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Typography, Box } from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import CallIcon from "@mui/icons-material/Call";
 
 const Contact = () => {
-  return <div>contact</div>;
+  return (
+    <Box>
+      <Typography variant="body1">
+        <MailIcon /> testing123@gmail.com
+      </Typography>
+
+      <Typography variant="body1">
+        <CallIcon /> 000/000000
+      </Typography>
+    </Box>
+  );
 };
 
 export default Contact;
