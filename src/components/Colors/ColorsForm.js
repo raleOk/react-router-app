29f3dbe64dabb4dbee5203f89f1cdeb813e34e30
Rastko Zivkovic <rale.pablito@gmail.com>
2022-01-22 13:22:13 +0100

    added better validation with proper error and ui updates

--- src/components/Colors/ColorsForm.js
+++ src/components/Colors/ColorsForm.js
@@ -6,6 +6,12 @@ import { nanoid } from "nanoid";
 const ColorsForm = () => {
   const [color, setColor] = useState({});
 
+  const [inputErr, setInputErr] = useState(false);
+  const [nameErr, setNameErr] = useState(false);
+  const [yearErr, setYearErr] = useState(false);
+  const [hexErr, setHexErr] = useState(false);
+  const [panErr, setPanErr] = useState(false);
+
   const navigate = useNavigate();
   const id = nanoid();
 
@@ -15,36 +21,70 @@ const ColorsForm = () => {
     });
   };
 
-  const colorValidation = () => {
+  const inputValidation = () => {
+    if (
+      color &&
+      Object.keys(color).length === 0 &&
+      Object.getPrototypeOf(color) === Object.prototype
+    ) {
+      setInputErr(true);
+      return false;
+    } else {
+      setInputErr(false);
+      return true;
+    }
+  };
+
+  const nameValidation = () => {
+    if (color.name === undefined || color.name.length < 2) {
+      setNameErr(true);
+      return false;
+    } else {
+      setNameErr(false);
+      return true;
+    }
+  };
+
+  const yearValidation = () => {
+    if (color.year === undefined || Number(color.year) <= 1990) {
+      setYearErr(true);
+      return false;
+    } else {
+      setYearErr(false);
+      return true;
+    }
+  };
+
+  const hexValidation = () => {
     const regX = /[0-9A-Fa-f]{6}/g;
-    for (let i in color) {
-      if (color[i] === "") {
-        return false;
-      }
-    }
-    if (color.name.length < 2) {
-      alert("Name must be longer than 2 characters!");
-      return false;
-    }
-    if (Number(color.year) < 1990) {
-      alert("Year must be after 1990!");
-      return false;
-    }
     regX.lastIndex = 0;
     if (!regX.test(color.color)) {
-      alert("Enter a valid Hexadecimal value!");
+      setHexErr(true);
       return false;
+    } else {
+      setHexErr(false);
+      return true;
     }
-    if (color.pantone_value[2] !== "-") {
-      alert("Invalid pantone value format! (eg. 17-2901)");
+  };
+
+  const panValidation = () => {
+    if (color.pantone_value === undefined || color.pantone_value[2] !== "-") {
+      setPanErr(true);
       return false;
+    } else {
+      setPanErr(false);
+      return true;
     }
-    return true;
   };
 
   const addColor = () => {
-    const validate = colorValidation();
-    if (validate) {
+    const nameVal = nameValidation();
+    const yearVal = yearValidation();
+    const hexVal = hexValidation();
+    const panVal = panValidation();
+    const inputVal = inputValidation();
+
+    if (nameVal && yearVal && hexVal && panVal && inputVal) {
       setColor(prevState => {
         return { ...prevState, id };
       });
@@ -67,8 +107,9 @@ const ColorsForm = () => {
         <Typography
           variant="h5"
           sx={{ mb: 3, mt: 3, fontWeight: "fontWeightLight" }}
+          color={inputErr ? "red" : "black"}
         >
-          Add a new color!
+          {inputErr ? "Fill out all fields please!" : "Add a new color!"}
         </Typography>
       </Container>
 
@@ -77,6 +118,8 @@ const ColorsForm = () => {
           name="name"
           label="Name"
           sx={{ mt: 1, mb: 2 }}
+          error={nameErr}
+          helperText={nameErr ? "Name must be longer than 2 characters!" : ""}
           onChange={changeHandler}
         />
       </Container>
@@ -86,6 +129,8 @@ const ColorsForm = () => {
           name="year"
           label="Year"
           sx={{ mt: 1, mb: 2 }}
+          error={yearErr}
+          helperText={yearErr ? "Year must be after 1990!" : ""}
           onChange={changeHandler}
         />
       </Container>
@@ -95,6 +140,8 @@ const ColorsForm = () => {
           name="color"
           label="Hexadecimal value"
           sx={{ mt: 1, mb: 2 }}
+          error={hexErr}
+          helperText={hexErr ? "Enter a valid Hexadecimal value!" : ""}
           onChange={changeHandler}
         />
       </Container>
@@ -104,6 +151,10 @@ const ColorsForm = () => {
           name="pantone_value"
           label="Pantone value"
           sx={{ mt: 1, mb: 2 }}
+          error={panErr}
+          helperText={
+            panErr ? "Invalid pantone value format! (eg. 17-2901)" : ""
+          }
           onChange={changeHandler}
         />
       </Container>
